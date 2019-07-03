7601674bc03f52806d5e4e476649e2f498539429
TATADELVAL <52458375+TATADELVAL@users.noreply.github.com>
2019-07-03 16:44:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,27 @@
 // JAVASCRIPT POUR LA PAGE REGLES
 function Reg_img1() {
   document.getElementById("image").src="Images/chat.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
-  document.getElementById("text").innerHTML="toto";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
+  document.getElementById("text").innerHTML="<h1>Le déroulement du jeu</h1><p>Le jeu d'échec se joue à deux joueurs qui font évoluer seize pièces chacun, respectivement blanches et noires, sur un échiquier de 64 cases en alternance blanches et noires. Pour parler des adversaires, on dit « les Blancs » et « les Noirs » </br> Au début d’une partie d’échecs, ce sont toujours les blancs qui commencent. Lors d’un tour de jeu, le joueur peut effectuer soit un déplacement, soit une prise soit un coup spécial.</p>";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
+}
+
+function Reg_img2() {
+  document.getElementById("image").src="Images/chat.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("text").innerHTML="  ";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
+}
+
+function Reg_img3() {
+  document.getElementById("image").src="Images/chat.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("text").innerHTML="  ";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
+}
+
+function Reg_img4() {
+  document.getElementById("image").src="Images/chat.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("text").innerHTML="  ";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
+}
+
+function Reg_img5() {
+  document.getElementById("image").src="Images/chat.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("text").innerHTML="  ";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
 
 // JAVASCRIPT POUR LA PAGE DEPLACEMENT DES PIECES
