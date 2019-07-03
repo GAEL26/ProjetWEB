c9d4d88a8fd6d6928bccf1f590a3fe36e6544ab5
GAEL26 <52463824+GAEL26@users.noreply.github.com>
2019-07-03 15:36:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 // JAVASCRIPT POUR LA PAGE REGLES
 function Reg_img1() {
-  document.getElementById("image").src="Images/test.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("image").src="Images/chat.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
   document.getElementById("text").innerHTML="toto";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
 
 // JAVASCRIPT POUR LA PAGE DEPLACEMENT DES PIECES
 
 function Dep_img1() {
-  document.getElementById("image").src="Images/test.gif";   //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("image").src="Images/chat.gif";   //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
   document.getElementById("text").innerHTML="toto";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
