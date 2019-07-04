1e27f8c504406537a51f02ad10dc69802f85a25e
TATADELVAL <52458375+TATADELVAL@users.noreply.github.com>
2019-07-04 09:29:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 
 // JAVASCRIPT POUR LA PAGE REGLES
 function Reg_img1() {
-  document.getElementById("image").src="imageregle1.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("image").src="Images/imageregle1.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
   document.getElementById("text").innerHTML="<h1>Le déroulement du jeu</h1><p>Le jeu d'échec se joue à deux joueurs qui font évoluer seize pièces chacun, respectivement blanches et noires, sur un échiquier de 64 cases en alternance blanches et noires. Pour parler des adversaires, on dit « les Blancs » et « les Noirs » </br> Au début d’une partie d’échecs, ce sont toujours les blancs qui commencent. Lors d’un tour de jeu, le joueur peut effectuer soit un déplacement, soit une prise soit un coup spécial.</p>";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
 function Reg_img2() {
-  document.getElementById("image").src="imageregle2.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("image").src="Images/imageregle2.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
   document.getElementById("text").innerHTML="<h1>Le but du jeu</h1><p>Ces différents déplacements sont proposés aux joueurs pour parvenir à réaliser leur objectif qui est de prendre le roi adverse. Lorsque ce but est atteint, le joueur qui y est parvenu, gagne immédiatement la partie qui prend fin automatiquement.</p>";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
 function Reg_img3() {
-  document.getElementById("image").src="imageregle3.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("image").src="Images/imageregle3.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
   document.getElementById("text").innerHTML="<h1>Echec</h1><p>Chaque fois que vous pouvez prendre le roi de votre adversaire à votre prochain coup, vous devez le prévenir en annonçant « échec ». Il doit alors tenter de parer votre échec, lors de son tour de jeu.</p>";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
 function Reg_img4() {
-  document.getElementById("image").src="imageregle4.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("image").src="Images/imageregle4.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
   document.getElementById("text").innerHTML="<h1>Echec et mat</h1><p>S’il n’y parvient pas, son roi est alors « échec et mat ». Vous remportez donc la partie.</p>";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
 function Reg_img5() {
-  document.getElementById("image").src="imageregle5.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("image").src="Images/imageregle5.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
   document.getElementById("text").innerHTML="<h1>Pat</h1><p>Une partie d’échecs peut également se terminer sur un match nul. Si lors du tour d’un joueur, celui-ci ne peut déplacer aucune de ses pièces et que son roi n’est pas en échec. On dit alors que la partie se termine par un « pat ».</p>";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
 
