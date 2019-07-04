b16823511e434b1611cb51d6ec3949fdedf7d3ee
TATADELVAL <52458375+TATADELVAL@users.noreply.github.com>
2019-07-04 09:52:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function Reg_img4() {
   document.getElementById("text").innerHTML="<h1>Echec et mat</h1><p>S’il n’y parvient pas, son roi est alors « échec et mat ». Vous remportez donc la partie.</p>";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
 function Reg_img5() {
-  document.getElementById("image").src="Images/imageregle5.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
-  document.getElementById("text").innerHTML="<h1>Pat</h1><p>Une partie d’échecs peut également se terminer sur un match nul. Si lors du tour d’un joueur, celui-ci ne peut déplacer aucune de ses pièces et que son roi n’est pas en échec. On dit alors que la partie se termine par un « pat ».</p>";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
+  document.getElementById("image").src="Images/Kasparov-topalov1999.gif";  //ECRIRE ENTRE LES GUILLEMETS LE NOM DU FICHIER image
+  document.getElementById("text").innerHTML="<h1>Pat</h1><p>Une partie d’échecs peut également se terminer sur un match nul. Si lors du tour d’un joueur, celui-ci ne peut déplacer aucune de ses pièces et que son roi n’est pas en échec. On dit alors que la partie se termine par un « pat<:br>Ci-contre partie légendaire 1999 Garry Kasparov (en blanc) contre Veselin Topalov».</p>";   //ECRIRE ENTRE LES GUILLEMETS LE TEXTE A AFFICHER
 }
 
 // JAVASCRIPT POUR LA PAGE DEPLACEMENT DES PIECES
